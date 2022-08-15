305f72b34c896d18948642cd186e71ceb81bfbb4
Alexis Bonetti <alexis.bonetti@gmail.com>
2022-08-15 18:57:22 -0300

    localStorage

--- basePerros.js
+++ basePerros.js
@@ -1,8 +1,10 @@
 const perros = []
-perros.push(new Adopcion ('Sudan', 'labrador', 'grande', 'amarillo','macho',false, false)),
-perros.push(new Adopcion ('Berna', 'pitbull', 'mediano', 'blanco atigrado','hembra',false ,false)),
-perros.push(new Adopcion ('Pluto', 'bulldog', 'pequeño', 'gris','macho',true , false)),
+perros.push(new Adopcion ('Sudan', 'labrador', 'grande', 'amarillo','macho',false, false));
+perros.push(new Adopcion ('Berna', 'pitbull', 'mediano', 'blanco atigrado','hembra',false ,false));
+perros.push(new Adopcion ('Pluto', 'bulldog', 'pequeño', 'gris','macho',true , false));
 perros.push(new Adopcion ('Nala', 'gran danes', 'mediano', 'negro-blanco','hembra',true, false));
+perros.push(new Adopcion ('Cachito', 'cruza', 'pequeño', 'marron','macho',false, false));
+
 console.log(perros)
 
 
@@ -13,6 +15,7 @@ const tabla = document.getElementById("perros");
 const buscar = document.querySelector("#buscar");
 const eliminar = document.querySelector("#eliminar");
 let tamaños = document.getElementById("selectTamanos");
+let setBusquedaPerros
 
 
 function newRow (newPerro){
@@ -36,43 +39,41 @@ function newRow (newPerro){
     tabla.append(row)
 };
 
-
+function listadoUpdate() {
+    tabla.innerHTML = "";  
+    
+        setBusquedaPerros = JSON.parse(localStorage.getItem("setBusquedaPerros"));
+        let selectorTamanos = localStorage.getItem("setSelectorTamanos");
+        tamaños.value = selectorTamanos
+        console.log(setBusquedaPerros)
+        setBusquedaPerros.forEach((item) => {
+            newRow(item);
+        });    
+        
+};
 
 //desarrollar el filtro por tamaños !
 function seleccionDeTamanos(){
-    let tamañosElegido = selectTamanos.value;
-    console.log(tamañosElegido)
+    tamañosElegido = tamaños.value;
+    localStorage.setItem("setSelectorTamanos",tamañosElegido)
+    console.log(tamañosElegido);
        
     if (tamañosElegido != "todos"){busquedaPerros = perros.filter(Adopcion => Adopcion.tamaño == tamañosElegido )} else{busquedaPerros= perros.slice()}
     console.log(busquedaPerros) ;
     
 }
 
+if(localStorage.getItem("setBusquedaPerros")!=null){
+    listadoUpdate();
+}
+
 buscar.addEventListener("click",(e)=>{
     e.preventDefault();
-    seleccionDeTamanos()
-    //busquedaPerros= perros.slice();
-    listadoUpdate()
+    seleccionDeTamanos();
+    localStorage.setItem("setBusquedaPerros",JSON.stringify(busquedaPerros))
+    listadoUpdate();
 });
 
-eliminar.onclick = () => {
-    busquedaPerros=[];
-    listadoUpdate();
+eliminar.onclick = () => {  
+    tabla.innerHTML= ""
 };
-
-
-/* tamaños.addEventListener("click",(e)=>{
-    e.preventDefault();
-    seleccionDeTamanos();
-    listadoUpdate()
-}); */
-
-function listadoUpdate() {
-    tabla.innerHTML = "";     
-    busquedaPerros.forEach((item) => {
-        newRow(item);
-    });    
-};
-
-
-
